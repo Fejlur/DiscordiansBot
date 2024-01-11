6711bbe228c1ba545e066ddd59628cecf9dcdc68
Fejlur <fejlur2137@gmail.com>
2024-01-11 17:30:58 +0100

    potential coc_on_war fix [typo fix]

--- main.py
+++ main.py
@@ -102,9 +102,9 @@ async def coc_member_leave(member, clan):
 @coc.WarEvents.war_attack()
 async def coc_new_attack(attack, war):
     ch = bot.get_channel(bot.war_logs_channel_id)
-    homeclan = bot.coc_client.get_clan(credentials["clan_tag"]
+    homeclan = bot.coc_client.get_clan(credentials["clan_tag"])
     logging.critical(f"attacker: {attack.attacker.clan} {attack.attacker.name} | defender: {attack.defender.clan} {attack.defender.name}")
-    if attack.attacker.clan.tag == homeclan.tag):
+    if attack.attacker.clan.tag == homeclan.tag:
         e = discord.Embed(title="New attack", description=f"{attack.stars} ⭐ {attack.destruction}%", colour=0xfc0303)
     else:
         e = discord.Embed(title="New defense",description=f"{attack.stars} ⭐ {attack.destruction}%", colour=0x03fc77)
